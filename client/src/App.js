f082f5a86342e72ec604f6f318aaacfa060596ea
emmanueljucaban <emmanueljucaban@gmail.com>
2020-04-28 10:01:45 -0700

    Push to master

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
+// Functional Components    Dumb Component
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const purpleMode = true;
+
+const RenderListItems = props => {
+  return props.items.map((item, index) => {
+    return <li key={index}>{item}</li>;
+  });
 }
+
+const Greetings = props => (
+  <div>
+    <h1>Hello World</h1>
+    <p>Hi my name is { purpleMode ? props.name : 'Emmanuel'}</p>
+    <p>{props.lastName}</p>
+    <ul>
+      <p>My favorite things to do in quarantine</p>
+      <RenderListItems items={["Play videogames", 'Sleep', 'Code']}/>
+    </ul>
+    <ul>
+      <p>My favorite movies to watch during the pandemic</p>
+      <RenderListItems items={["Pulp Fiction", "John Wick", "Harry Potter"]}/>
+    </ul>
+  </div>
+);
+
+
+const App = props =>  (
+  <div className="colorBlue" style={{ fontSize: '40px', backgroundColor: purpleMode ? 'purple' : 'green'}}>
+    <Greetings name={props.name} lastName={props.lastName}/>
+    <Greetings/>
+  </div>
+);
+
+
 
 export default App;
